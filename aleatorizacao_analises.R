3c63b13d619f8567a0a3a370d853d04aecd7eb2a
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-05-11 15:11:09 -0300

    add 1 person to fulfill all the duos

--- aleatorizacao_analises.R
+++ aleatorizacao_analises.R
@@ -6,6 +6,8 @@
 
 library(readxl)
 library(dplyr)
+library(writexl)
+
 dados = read_xlsx("matriculados_musica_20181.xlsx")
 
 dados$PERÍODO = as.integer(dados$PERÍODO)
@@ -22,6 +24,20 @@ turma$aleatorizacao = aleatorizacao
 
 selecao = turma %>% arrange(aleatorizacao) %>% select(aleatorizacao, NOME, PERÍODO) %>% 
   mutate(tratamento = c(rep(1, times = nrow(turma)%/%2), rep(0, nrow(turma)%/%2), 0))
+#View(selecao)
+
+#nrow(selecao)
+
+# Selecionando mais um caso aleatoriamente para compor a ultima dupla
+set.seed(100)
+mais_um = selecao %>% sample_n(1)
+mais_um
+
+selecao = rbind(selecao, mais_um)
+
+nrow(selecao)
+selecao$aleatorizacao[60] = NA
+selecao$tratamento[60] = 0
 View(selecao)
 
-
+write_xlsx(selecao, "selecao_duplas_tcc_kelly.xlsx")
