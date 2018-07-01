1c64e30f6b83524e58b307fe2ed35b583e2d3752
Neylson Crepalde <neylsoncrepalde@gmail.com>
2018-07-01 14:04:54 -0300

    update script to generate results tables

--- resultados.R
+++ resultados.R
@@ -6,6 +6,7 @@
 
 library(readr)
 library(dplyr)
+library(xtable)
 
 dados = read_csv("tcc_kelly_dados.csv")
 dados
@@ -16,11 +17,16 @@ t.test(dados$nt_dinamica ~ dados$tratamento)
 t.test(dados$nt_diminuendo ~ dados$tratamento)
 
 
-t.test(dados$nt_afinacao ~ dados$tratamento, alternative = "less")
-t.test(dados$nt_articulacao ~ dados$tratamento, alternative = "less")
-t.test(dados$nt_dinamica ~ dados$tratamento, alternative = "less")
-t.test(dados$nt_diminuendo ~ dados$tratamento, alternative = "less")
+t1 = t.test(dados$nt_afinacao ~ dados$tratamento, alternative = "less")
+t2 = t.test(dados$nt_articulacao ~ dados$tratamento, alternative = "less")
+t3 = t.test(dados$nt_dinamica ~ dados$tratamento, alternative = "less")
+t4 = t.test(dados$nt_diminuendo ~ dados$tratamento, alternative = "less")
 
+
+xtable(cbind(c(t1$statistic, t1$parameter, t1$p.value),
+             c(t2$statistic, t2$parameter, t2$p.value),
+             c(t3$statistic, t3$parameter, t3$p.value),
+             c(t4$statistic, t4$parameter, t4$p.value)))
 
 # Tentando com um score único
 library(FactoMineR)
@@ -35,8 +41,12 @@ scores
 
 dados$scores = scores
 t.test(dados$scores ~ dados$tratamento)
-t.test(dados$scores ~ dados$tratamento, alternative = "less")
+t5 = t.test(dados$scores ~ dados$tratamento, alternative = "less")
 
-
+xtable(cbind(c(t1$statistic, t1$parameter, t1$p.value),
+             c(t2$statistic, t2$parameter, t2$p.value),
+             c(t3$statistic, t3$parameter, t3$p.value),
+             c(t4$statistic, t4$parameter, t4$p.value),
+             c(t5$statistic, t5$parameter, t5$p.value)))
 
 
